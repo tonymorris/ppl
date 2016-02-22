e9af0a7d364d637ac8af6b8d93b84936f3f41f7a
Tony Morris <tmorris@tmorris.net>
2016-02-22 13:18:58 +1000

    Bob Tait pre-area solo practice exam

--- practice-exams.hs
+++ practice-exams.hs
@@ -2113,7 +2113,578 @@ taitPreSolo =
 
         [
         ]
+    ]
 
+taitPreAreaSolo ::
+  Exam
+taitPreAreaSolo =
+  Exam
+    "Bob Tait RPL Study Guide"
+    (Just "A study guide for the Recreational Pilot Licence")
+    (Just "Pre-Area Solo Practice Exam")
+    [
+      "The point about which pitch, roll and yaw occur during flight is the" ~>
+      Multichoice
+        [
+          "centre of pressure"
+        , "wing roots"
+        , "the mid point between the nose and the tail"
+        ]
+
+        "the centre of gravity"
+
+        [
+        ]
+    , "The location of the centre of gravity of an aeroplane" ~>
+      Multichoice
+        [
+          "is a fixed point determined by the manufacturer"
+        ]
+
+        "depends upon the load distribution within the aircraft"
+
+        [
+          "remains constant during any one flight"
+        , "moves forward if weight is added to the rear"
+        ]
+    , "The induced drag acting on aircraft in straight and level flight is" ~>
+      Multichoice
+        [
+        ]
+
+        "greatest when speed is low and the aircraft is heavy"
+
+        [
+          "greatest when speed is high and the aircraft is heavy"
+        , "greatest when speed is low and the aircraft is light"
+        , "greatest when speed is high and the aircraft is light"
+        ]
+    , "Which of the following would increase the parasite drag on an aircraft?" ~>
+      Multichoice
+        [
+          "raising the flaps"
+        , "retracting the undercarriage"
+        ]
+
+        "increasing the IAS"
+
+        [
+          "decreasing the IAS"
+        ]
+    , "The least amount of total drag in level flight occurs" ~>
+      Multichoice
+        [
+          "at the high end of the speed range at about cruising speed"
+        , "when the aircraft is flying as slowly as possible"
+        ]
+
+        "at a speed about equal to the best gliding speed, with the flaps retracted"
+
+        [
+          "at a speed about equal to the best gliding speed, with the flaps extended"
+        ]
+    , "The IAS which procudes the best gliding performance in still air is" ~>
+      Multichoice
+        [
+          "the IAS which produces maximum lift"
+        ]
+
+        "the IAS which produces the best lift/drag ratio"
+
+        [
+          "the IAS which requires least control deflection"
+        , "the IAS which results from a level nose attitude"
+        ]
+    , "If flap is lowered while maintaining level flight at constant power" ~>
+      Multichoice
+        [
+        ]
+
+        "the nose attitude will be lower and the resulting IAS will be lower"
+
+        [
+          "the nose attitude will be higher and the resulting IAS will be lower"          
+        , "the nose attitude will be lower and the resulting IAS will be higher"
+        , "the nose attitude will be higher and the resulting IAS will be higher"
+        ]
+    , "If two aircraft identical in every respect except for weight are flying at the same height and IAS," ~>
+      Multichoice
+        [
+        ]
+
+        "the heavier aircraft will require a higher nose attitude and more power"
+
+        [
+          "the heavier aircraft will require a lower nose attitude and more power"
+        , "the heavier aircraft will require a lower nose attitude and less power"
+        , "both aircraft will require the same nose attitude and power"
+        ]
+    , "Which of the following factors will increase the angle of climb of an aircraft?" ~>
+      Multichoice
+        [
+          "an increase in weight"        
+        ]
+
+        "an increase in headwind"
+
+        [
+          "the use of full flap"
+        , "climbing at the slowest possible speed will full power"
+        ]
+    , "To achieve the best rate of climb in any given wind the climb should be made" ~>
+      Multichoice
+        [
+          "at an IAS lower than that required for best angle with full power applied"
+        , "at the same IAS as is required for best angle, but with less than full power"
+        , "at an IAS higher than that required for best angle, but with less than full power"
+        ]
+
+        "at an IAS higher than that required for best angle with full power applied"
+
+        [
+        ]
+    , "What effect will turning have on climb performance?" ~>
+      Multichoice
+        [
+          "the angle of climb will decrease but the rate of climb will increase"
+        ]
+
+        "both angle and rate of climb with decrease"
+
+        [
+          "both angle and rate of climb with increase"
+        , "the angle of climb will increase but the rate of climb will decrease"
+        ]
+    , "An aircraft is flying level at 5000 feet at constant power setting in a tail wind. Which of the following is true?" ~>
+      Multichoice
+        [
+        ]
+
+        "the IAS is less than the TAS"
+
+        [
+          "the IAS is higher than the TAS"
+        , "the IAS is greater than the GS"
+        , "the TAS is higher than the GS"
+        ]
+    , "An aircraft is carrying out a level balanced turn at 60 degrees angle of bank. Which of the following would be correct?" ~>
+      Multichoice
+        [
+          "lift is equal to weight"
+        ]
+
+        "lift is twice the weight"
+
+        [
+          "lift is three times weight"
+        , "lift is 1.4 times weight"
+        ]
+    , "An aircraft stalls at 50 KIAS in level flight at a particular weight and power setting. At the same weight and power setting, in a 60 degree bank level turn the stalling speed would be closest to" ~>
+      Multichoice
+        [
+        ]
+
+        "70 KIAS"
+
+        [
+          "50 KIAS"
+        , "60 KIAS"
+        , "100 KIAS"
+        ]        
+    , "The elevator control is abruptly pulled to te fully aft position to induce a stall" ~>
+      Multichoice
+        [
+        ]
+
+        "the stall will occur at a higher speed than normal"
+
+        [
+          "the stall will occur at a lower speed than normal"
+        , "the stall will occur at the same speed but a higher angle of attack than normal"
+        , "the stall will occur at the same speed and same angle of attack as normal"
+        ]
+    , "Which of the following factors has no effect on the stalling IAS of an aircraft?" ~>
+      Multichoice
+        [
+          "weight"
+        , "load factor"
+        , "power"
+        ]
+
+        "height"
+
+        [
+        ]                
+    , "Where in relation to a heavy aircraft will wake turbulence be worst?" ~>
+      Multichoice
+        [
+          "behind and above the heavy aircraft"
+        ]
+
+        "behind and below the heavy aircraft"
+
+        [
+          "in front of and below the heavy aircraft"
+        , "in front of anf above the heavy aircraft"
+        ]                
+    , "Which of the following control difficulties is most likely to be associated with an encounter with wake turbulence?" ~>
+      Multichoice
+        [
+          "a violent pitch"
+        , "a violent pitch and yaw"
+        , "a violent yaw"
+        ]
+
+        "a violent roll"
+
+        [
+        ]                
+    , "The best way to avoid the wake turbulence behind a heavy aircraft which has just landed is to" ~>
+      Multichoice
+        [
+          "aim to touch down at point before the touch-down point of the heavy aircraft"
+        , "aim to touch down at the same point as the heavy aircraft"
+        ]
+
+        "aim to touch down at a point ahead of the touch-down point of the heavy aircraft"
+
+        [
+          "aim to touch down behind the heavy aircraft but on one side of the runway"
+        ]                
+    , "Which of the following would result in a decrease in the rate of climb?" ~>
+      Multichoice
+        [
+        ]
+
+        "a reduction in engine power output"
+
+        [
+          "an increase in headwind component"
+        , "a decrease in headwind component"
+        , "a reduction in gross weight"
+        ]                
+    , "The purpose of a carburettor is" ~>
+      Multichoice
+        [          
+        ]
+
+        "to mix the correct amount of air with fuel to ensure efficient combustion"
+
+        [
+          "to remove any dust particles from the air on its way to the engine"
+        , "to remove any water from the fuel before it can get to the engine"
+        , "to cool the air on its way to the engine to prevent detonation"
+        ]                
+    , "Under what circumstances can carburettor ice form?" ~>
+      Multichoice
+        [
+          "only when the outside air temperature is below zero"
+        , "only if the aircraft is flying in rain or drizzle"
+        ]
+
+        "only at low power settings in cold moist air"
+
+        [
+          "over a very wide range of temperatures, both inside and outside of cloud"
+        ]                
+    , "Under which of the following circumstances would carburettor ice be difficult to detect?" ~>
+      Multichoice
+        [
+          "at full power during take-off"
+        , "during a long climb"
+        ]
+
+        "during a long descent at low power"
+
+        [
+          "during cruise just below a cloud base"
+        ]                
+    , "What is the correct method of using carburettor heat during a long power-off glide?" ~>
+      Multichoice
+        [
+          "carburettor heat should be applied only if carburettor ice is suspected"
+        ]
+
+        "carburettor heat should be applied before the throttle is closed and it should be left on during the glide"
+
+        [
+          "carburettor heat should be applied just before power is re-applied at the end of the glide"
+        , "carburettor heat should be applied regularly for brief intervals during the glide"
+        ]                
+    , "Carburettor icing during cruise will be indicated to the pilot by" ~>
+      Multichoice
+        [
+        ]
+
+        "a drop in RPM, a reduction in cruise speed and rough running"
+
+        [
+          "no change in RPM, but a reduction in cruise speed and a height loss"
+        , "the engine back firing when power is increased"
+        , "increasing RPM even though there has been no change in the throttle position"
+        ]
+    , "If an aircraft fitted with a carburettor and a gravity feed fuel system has a blockage in the fuel tank vent" ~>
+      Multichoice
+        [
+          "there will be less power available during take-off"
+        , "the engine will be very difficult to start"
+        ]
+
+        "the engine may fail during flight due to fuel starvation"
+
+        [
+          "the engine may run roughly due to an excessively rich mixture"
+        ]
+    , "Detonation in an aircraft engine is most likely when" ~>
+      Multichoice
+        [
+        ]
+
+        "both the engine and fuel are hot and high power is applied"
+
+        [
+          "full power is applied during take-off with the mixture fully rich"
+        , "the throttle is opened after a long descent"
+        , "the aircraft is cruising at high power for a long period of time"
+        ]
+    , "If detonation is suspected during a long climb you should" ~>
+      Multichoice
+        [
+          "reduce the power and raise the nose to climb at a lower speed"
+        , "increase the power and raise the nose to climb at the same airspeed"
+        , "reduce the power and raise the nose to increase rate of climb"
+        ]
+
+        "reduce the power, check that the mixture is fully rich and lower the nose to climb at a higher airspeed"
+
+        [
+        ]
+    , "Which of the following instruments would be affected by a failure of the vaccuum pump?" ~>
+      Multichoice
+        [
+          "the altimeter"
+        , "the vertical speed indicator"
+        , "the airspeed indicator"
+        ]
+
+        "the artificial horizon"
+
+        [
+        ]
+    , "If the altimeter fails to indicate any height change after take-off the most likely cause is" ~>
+      Multichoice
+        [
+          "the vaccuum pump has failed"
+        , "the pitot tube is blocked"
+        ]
+
+        "the static vent is blocked"
+
+        [
+          "the incorrect QNH was set before take-off"
+        ]
+    , "If your aircraft was accidentally refuelled with jet fuel [AVTUR] instead of the correct type of fuel" ~>
+      Multichoice
+        [
+          "you would notice a big increase in power during take-off"
+        ]
+
+        "you would experience a significant power loss accompanied by rough running, detonation and high engine temperatures"
+
+        [
+          "there would be no particular effect as long as the tank contains less than half AVTUR"
+        , "there would be a slight reduction in power but you could continue with the proposed flight"
+        ]
+    , "If you take off from an uncontrolled airstrip which has left hand circuits, what is the earliest point after take-off at which you may turn right?" ~>
+      Multichoice
+        [
+          "500 feet AGL"
+        , "500 metres from the aerodrome boundary"
+        , "1000 feet AGL"
+        ]
+
+        "1500 feet AGL or 3 nautical miles from the aerodrome boundary"
+
+        [
+        ]
+    , "The lowest height at which you may fly in the training area is" ~>
+      Multichoice
+        [
+        ]
+
+        "1000 ft over a town or populous area otherwise 500ft"
+
+        [
+          "1500 ft over a town or populous area otherwise 500ft"
+        , "500 ft over a town or populous area otherwise 1000ft"
+        , "200 ft provided you are practising forced landings"
+        ]
+    , "With regard to the end of daylight, you must leave the training area in time to land" ~>
+      Multichoice
+        [
+          "30 minutes before the end of daylight"
+        ]
+
+        "10 minutes before the end of daylight"
+
+        [
+          "by the end of daylight"
+        , "5 minutes after the end of daylight"
+        ]
+    , "When turning from upwind to downwind at low level on a very windy day, you may experience the illusion of" ~>
+      Multichoice
+        [
+          "skidding out"
+        ]
+
+        "slipping in"
+
+        [
+          "overbanking"
+        , "underbanking"
+        ]
+    , "Where may a student pilot **not** fly solo?" ~>
+      Multichoice
+        [
+          "beyond the circuit area of the aerodrome"
+        ]
+
+        "beyond circuit area of the aerodrome or beyond the associated training area"
+
+        [
+          "within the training area"
+        , "flying the most direct route between the training area and the aerodrome"
+        ]
+    , "What is the maximum number of consecutive solo hours that a student pilot may fly?" ~>
+      Multichoice
+        [
+          "2"
+        , "10"
+        ]
+
+        "3"
+
+        [
+          "5"
+        ]
+    , "When must a fuel inspection be carried out?" ~>
+      Multichoice
+        [
+          "before every flight"
+        , "after each refuelling"
+        , "before the first flight of each day"
+
+        ]
+
+        "after each refuelling and before the first flight of the day"
+
+        [
+        ]
+    , "When operating at 2000 ft AMSL in your local training area, which of the following would be the minimum requirement for maintaining VMC?" ~>
+      Multichoice
+        [
+          "500 ft below the cloud base and visibility of 8 km"
+        , "500 ft below the cloud base and visibility of 5 km"
+        ]
+
+        "clear of cloud and visibility of 5 km"
+
+        [
+          "clear of cloud with no special requirement for visibility"
+        ]
+    , "What is the longest time since your last flight which would allow you to fly solo today without a dual check?" ~>
+      Multichoice
+        [
+          "7 days"
+        , "30 days"
+        , "90 days"
+        ]
+
+        "14 days"
+
+        [
+        ]        
+    , "State the IAS you would use for climb-out and approach at a short field\n  * Short field take-off climb-out speed\n  * Short field landing approach speed" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "Answer the following questions with regard to the fuel system in your aeroplane.\n  * Does it have a carburettor or is it fuel-injected?\n  * Is the system gravity fed or is it pump fed?\n  * Would your fuel system continue to operate if the aeroplane were upside-down?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "[Electronics]\n  * What voltage is your electrical system?\n  * What indication would you have if the alternator failed in flight?\n  * What would you do if the alternator failed in flight?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "List the services you would lose if you suffered a loss of all electrical power." ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "[Brakes]\n  * What would be the symptoms of air in the brake lines?\n  * A leak in the brake lines?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "what is the minimum amount of fuel that must be on board your aircraft at start-up if you intend to carry out a one hour flight in the training area?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "What altimeter setting should be used for a flight in the training area?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "What is the IAS and flap setting that would provide best gliding range in your aircraft?" ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
+    , "While practising steep turns in your aircraft you notice that the airspeed is rapidly increasing past the green arc and the nose is dropping below the horizon in spite of your attempts to raise it by applying back elevator. List the actions, in order that you would take to recover from this situation." ~>
+      Multichoice
+        [
+        ]
+
+        "**RESEARCH THESE QUESTIONS FOR YOUR TRAINING AEROPLANE. CHECK WITH YOUR FLYING INSTRUCTOR.**"
+
+        [
+        ]
     ]
 
 exams ::
@@ -2124,6 +2695,7 @@ exams =
   , atcPreArea
   , atcBAK
   , taitPreSolo
+  , taitPreAreaSolo
   ]
 
 main ::
