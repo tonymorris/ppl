c63edd4b47d2c059a43a2d74fb22003cd513d0a0
Tony Morris <tmorris@tmorris.net>
2016-03-05 15:57:44 +1000

    airborne-aviation.com.au practice exam

--- practice-exams.hs
+++ practice-exams.hs
@@ -3280,6 +3280,520 @@ taitPreRPL =
         ]                
     ]
 
+airborneAviationPreSolo ::
+  Exam
+airborneAviationPreSolo =
+  Exam
+    "airborne-aviation.com.au Pre-Solo"
+    Nothing
+    (Just "PRESOLO01/PRESOLO02/PRESOLO03/PRESOLO04")
+    [
+      "What is the minimum height that an aircraft may commence a turn in the circuit direction after take-off?" ~>
+      Multichoice
+        [
+          "1000 ft AGL"
+        , "800 ft AGL"
+        ]
+
+        "500 ft AGL"
+
+        [
+          "No minimum unless specified by local regulations"
+        ]
+    , "If a balance ball is indicating right of centre you should apply:" ~>
+      Multichoice
+        [
+        ]
+
+        "More right rudder to centre the ball"
+
+        [
+          "More right aileron to centre the ball"
+        , "More left rudder to centre the ball"
+        , "More left aileron to centre the ball"
+        ]
+    , "If your aircraft was to enter a spiral dive to effect recovery you should:" ~>
+      Multichoice
+        [
+          "Pull back firmly to stop the descent"
+        ]
+
+        "Level the wings, reduce power, pull out of dive"
+
+        [
+          "Reduce power to idle, pull nose up, roll wings level"
+        , "Apply spin recovery technique"
+        ]
+    , "What effect will turning have on climb performance?" ~>
+      Multichoice
+        [
+        ]
+
+        "Both angle and rate of climb will decrease"
+
+        [
+          "Angle of climb will increase, but rate will decrease"
+        , "Both angle and rate of climb will increase"
+        , "Rate of climb will increase, but angle will decrease"
+        ]
+    , "If an aircraft has a stall speed (Vs) of 40 KIAS in level flight the stall speed during a level 60 degree angle of bank turn with all other variables remaining unchanged would be closest to:" ~>
+      Multichoice
+        [
+          "45 KIAS"
+        , "80 KIAS"
+        , "70 KIAS"
+        ]
+
+        "55 KIAS"
+
+        [
+        ]
+    , "What effect will turning have on climb performance?" ~>
+      Multichoice
+        [
+        ]
+
+        "Both angle and rate of climb will decrease"
+
+        [
+          "Angle of climb will increase, but rate will decrease"
+        , "Both angle and rate of climb will increase"
+        , "Rate of climb will increase, but angle will decrease"
+        ]
+    , "If an aircraft has a stall speed (Vs) of 40 KIAS in level flight the stall speed during a level 60 degree angle of bank turn with all other variables remaining unchanged would be closest to:" ~>
+      Multichoice
+        [
+          "45 KIAS"
+        , "80 KIAS"
+        , "70 KIAS"
+        ]
+
+        "55 KIAS"
+
+        [
+        ]
+    , "Under what circumstances is carburettor ice likely to form?" ~>
+      Multichoice
+        [
+        ]
+
+        "Temperatures below 25 degrees C at low power"
+
+        [
+          "In cold and moist air at low power"
+        , "Only below a temperature of 0 degrees C"
+        , "Only if the aircraft is flying in rain"
+        ]
+    , "Which of the following instruments might be affected by a vacuum system failure?" ~>
+      Multichoice
+        [
+          "Altimeter"
+        ]
+
+        "Attitude Indicator"
+
+        [
+          "Vertical Speed Indicator"
+        , "Airspeed Indicator"
+        ]
+    , "Which of the following would result in a decreased rate of climb?" ~>
+      Multichoice
+        [
+          "An increase in headwind component"
+        ]
+
+        "A reduction in engine power"
+
+        [
+          "A reduction in weight"
+        , "A decrease in headwind component"
+        ]
+    , "If a fuel gauge indicates 12 US gallons in a fuel tank and it is properly calibrated you would expect to see how many litres on your dipstick?" ~>
+      Multichoice
+        [
+          "54 litres"
+        , "48 litres"
+        ]
+
+        "45 litres"
+
+        [
+          "36 litres"
+        ]
+    , "If the elevator control is abruptly pulled fully aft at cruise speed - how does the aeroplane respond?" ~>
+      Multichoice
+        [
+        ]
+
+        "A stall will occur at a higher speed than normal"
+
+        [
+          "A stall will occur at a lower speed than normal"
+        , "A stall will occur at the same speed but at a higher angle of attack than normal"
+        , "The aircraft will not stall as it's flying faster than Vs, the aircraft climbs rapidly"
+        ]
+    , "In what circumstance would it be most appropriate to transmit a PAN call?" ~>
+      Multichoice
+        [
+          "One of your passengers just threw up on the back seat!"
+        , "Your engine has failed and you are planning on ditching in to water"
+        ]
+
+        "You are experiencing a rough running engine but have enough power to maintain level flight"
+
+        [
+          "Your vertical speed indicator has failed in flight"
+        ]
+    , "Unless assigned a discrete code by ATC for your transponder you should squawk which code when flying VFR?" ~>
+      Multichoice
+        [
+        ]
+
+        "1200"
+
+        [
+          "7700"
+        , "2100"
+        , "7600"
+        ]
+    , "Which of the following is a VHF frequency?" ~>
+      Multichoice
+        [
+          "281 KHz"
+        , "12.010 MHz"
+        , "13075 MHz"
+        ]
+
+        "124.55 MHz"
+
+        [
+        ]
+    , "Which factor listed below has no impact on the stalling IAS of an aeroplane?" ~>
+      Multichoice
+        [
+          "Weight"
+        ]
+
+        "Wind"
+
+        [
+          "Power setting"
+        , "Loading (CG)"
+        ]
+    , "During a glide approach at the recommended IAS for your aeroplane type if you were to raise the nose and slow the aeroplane in nil wind your gliding range would:" ~>
+      Multichoice
+        [
+        ]
+
+        "decrease and a steeper approach path would be flown"
+
+        [
+          "remain the same and approach path would be relatively unchanged"
+        , "increase and a flatter approach path would be flown"
+        , "remain the same however the slower speed would ensure the aeroplane remained airborne longer"
+        ]
+    , "When climbing, if one magneto was unserviceable, the effect on climb performance would be:" ~>
+      Multichoice
+        [
+          "Reduced angle of climb, rate unaffected"
+        , "Reduced rate of climb, angle unaffected"
+        ]
+
+        "Reduced rate and angle of climb"
+
+        [
+          "No effect"
+        ]
+    , "VNE (Velocity Never Exceed) is indicated on an airspeed indicator by:" ~>
+      Multichoice
+        [
+          "the end of the green arc"
+        , "not indicated - only found in flight manual or on cockpit placard"
+        , "the end of the yellow arc"
+        ]
+
+        "a red line"
+
+        [
+        ]
+    , "When slowing down on downwind in the circuit to follow a slower aircraft flap may be applied. This is primarily to:" ~>
+      Multichoice
+        [
+          "create more drag to oppose the thrust"
+        ]
+
+        "improve forward visibility"
+
+        [
+          "increase the lift/drag ratio"
+        , "raise the stall speed"
+        ]
+    , "You are in the circuit flying the base leg. You find yourself too close to an aircraft also on the base leg. The best course of action is to:" ~>
+      Multichoice
+        [
+        ]
+
+        "go-around from the base leg"
+
+        [
+          "commence an orbit and rejoin base"
+        , "overshoot final approach before rejoining final to provide extra space"
+        , "reduce your speed below the normal approach speed"
+        ]
+    , "If an aircraft had a fuel consumption for planning purposes of 35 litres per hour what is the minimum flight fuel required for a flight of 1 hour and 15 minutes including the mandatory fuel reserve?" ~>
+      Multichoice
+        [
+          "44 litres"
+        , "62 litres"
+        ]
+
+        "70 litres"
+
+        [
+          "79 litres"
+        ]
+    , "An aircraft must be established on a straight final approach path by:" ~>
+      Multichoice
+        [
+          "at a distance determined to be sensible by the pilot"
+        ]
+
+        "500 feet AGL"
+
+        [
+          "500 metres from the aerodrome boundary"
+        , "there is no restriction"
+        ]
+    , "What document contains specific aerodrome procedures and information?" ~>
+      Multichoice
+        [
+        ]
+
+        "ERSA"
+
+        [
+          "AIP-MAP"
+        , "CAO"
+        , "VFG"
+        ]
+    , "When must the flying controls be checked for full, free and correct movement on an aircraft?" ~>
+      Multichoice
+        [
+          "Before engine start"
+        , "During the take-off roll"
+        , "After engine start"
+        ]
+
+        "Immediately before take-off"
+
+        [
+        ]
+    , "You are listening to a radio transmission and hear the first part clearly, while the second part is a loud squealing noise. The most likely cause is:" ~>
+      Multichoice
+        [
+        ]
+
+        "two stations transmitting at the same time"
+
+        [
+          "a problem with your squelch adjustment"
+        , "normal interference as a result of solar radiation flares"
+        , "the electrical system in the aircraft is failing"
+        ]
+    , "If your engine fails shortly after take-off while on the upwind leg you should:" ~>
+      Multichoice
+        [
+          "execute an immediate turn back towards the runway you departed from"
+        ]
+
+        "lower the nose to maintain a safe airspeed and choose a landing site that requires minimal turning"
+
+        [
+          "raise the nose to extend your glide performance and select a field within range"
+        , "transmit a MAYDAY call immediately with details of your situation then decide upon a plan of action"
+        ]
+    , "You are on a taxiway and another aircraft approaches you head on. You must:" ~>
+      Multichoice
+        [
+        ]
+
+        "move to the right and pass with care"
+
+        [
+          "move only if you are the smaller aircraft"
+        , "move to the left and pass with care"
+        , "hold your position and flash your taxi light"
+        ]
+    , "The runway is occupied and you are required to go-around. The most correct sequence of actions is:" ~>
+      Multichoice
+        [
+          "Raise the nose to a climbing attitude, apply full power, retract the flaps"
+        , "Retract the flaps, raising the nose with power application"
+        , "Raise the nose to a climbing attitude, retract the flaps to reduce drag, apply full power"
+        ]
+
+        "Apply full power while raising the nose to a climbing attitude, raising the flap slowly when climbing"
+
+        [
+        ]
+    , "Compared to an approach at full flap, an approach with no flaps at the same IAS will require:" ~>
+      Multichoice
+        [
+          "a lower nose attitude and steeper approach path"
+        , "a lower nose attitude and flatter approach path"
+        , "a higher nose attitude and steeper approach path"
+        ]
+
+        "a higher nose attitude and flatter approach path"
+
+        [
+        ]
+    , "You are lining up behind another aircraft that has just landed on the runway you are planning to take-off from. You must not commence your take-off roll until the landing aircraft has:" ~>
+      Multichoice
+        [
+          "started to turn towards the runway exit"
+        , "stopped on the runway"
+        ]
+
+        "vacated the runway and is taxiing away"
+
+        [
+          "passed beyond where you feel your aircraft will be airborne"
+        ]
+    , "During an approach to land what would the symptoms of an impending stall be?" ~>
+      Multichoice
+        [
+          "Low power setting and falling nose attitude"        
+        ]
+
+        "Low airspeed, sluggish controls, high nose attitude"
+
+        [
+          "Low nose attitude at higher than normal power setting"
+        , "Aircraft tending to want to drop a wing at high power"
+        ]
+    , "In which of the following situations would you transmit a MAYDAY call?" ~>
+      Multichoice
+        [
+          "There is a sick passenger on board who asks you to land as soon as possible"
+        , "A bird strikes your aircraft causing minor damage"
+        ]
+
+        "The engine has failed in flight"
+
+        [
+          "You are unsure of your position with 2 hours fuel remaining"
+        ]
+    , "A pilot can establish that the elevator is correctly trimmed when:" ~>
+      Multichoice
+        [
+        ]
+
+        "the required aircraft attitude is maintained when the controls are released"
+
+        [
+          "the wings are level"
+        , "the trim position indicator is in the neutral position"
+        , "the aircraft is maintaining attitude"
+        ]
+    , "Airspeed has been allowed to drop significantly such that the pilot suspects a stall may be imminent. The immediate actions to rectify this situation are:" ~>
+      Multichoice
+        [
+          "power off, wings level, lower the nose"
+        , "add full power rapidly"
+        , "reduce power and raise the nose"
+        ]
+
+        "add power and lower the nose"
+
+        [
+        ]
+    , "At the windsock of an aerodrome a double cross marker is displayed, this means:" ~>
+      Multichoice
+        [
+          "the aerodrome is completely unserviceable"
+        ]
+
+        "gliding operations are in progress"
+
+        [
+          "unsealed surfaces should not be used"
+        , "CTAF procedures are in force"
+        ]
+    , "The circuit height at an aerodrome for a light piston engine aircraft by convention is:" ~>
+      Multichoice
+        [
+        ]
+
+        "1000 ft AGL"
+
+        [
+          "1300 ft AMSL"
+        , "1000 ft AMSL"
+        , "1500 ft AGL"
+        ]
+    , "During take-off in no wind it is necessary to apply right rudder in most aircraft to maintain a straight path on the runway - why is this?" ~>
+      Multichoice
+        [
+          "The propeller slipstream pushing on the left side of the rudder and vertical fin"
+        , "Engine torque"
+        , "The coriolis effect"
+        ]
+
+        "Both slipstream and torque as listed above"
+
+        [
+        ]
+    , "When setting QNH on the altimeter while parked on an aerodrome the altimeter will read:" ~>
+      Multichoice
+        [
+          "zero feet"
+        ]
+
+        "height above sea level"
+
+        [
+          "height in a standard atmosphere"
+        , "250 feet"
+        ]
+    , "You are heading for a night out at the pub on Friday. You have a flying lesson booked for 7am Saturday morning, when must you stop drinking alcohol?" ~>
+      Multichoice
+        [
+          "It is dependant on the amount you drink, you must not return a blood alcohol reading of more than 0.02"
+        , "1:00am Saturday and ensure you are not affected by alcohol at the time of your flight"
+        , "You must not drink the day before your flight"
+        ]
+
+        "11:00pm Friday and ensure you are not affected by alcohol at the time of your flight"
+
+        [
+        ]
+    , "When taxiing you must always give way to aircraft" ~>
+      Multichoice
+        [
+          "that are larger than your aircraft"
+        ]
+
+        "on your right and those exiting a runway"
+
+        [
+          "on your right or any aircraft displaying strobes"
+        , "on your left and those exiting a runway"
+        ]
+    , "The high speed end of the white arc on an airspeed indicator (ASI) shows:" ~>
+      Multichoice
+        [
+        ]
+
+        "the maximum IAS at which flaps may be lowered"
+
+        [
+          "the preferred speed for approach and landing"
+        , "VNE - velocity never exceed"
+        , "the minimum IAS at which flaps may be retracted"
+        ]
+    ]
+
 airborneAviationAreaSolo ::
   Exam
 airborneAviationAreaSolo =
@@ -3648,7 +4162,7 @@ airborneAviationAreaSolo =
         [
           "72 litres"
         ]                                               
-    , "The ATIS reports that the QNH is 1015 and temperature is 25°C at an airport where the elevation is 250ft. When QNH is set on the subscale of your altimeter it should:" ~>
+    , "The ATIS reports that the QNH is 1015 and temperature is 25 degrees C at an airport where the elevation is 250ft. When QNH is set on the subscale of your altimeter it should:" ~>
       Multichoice
         [
         ]
@@ -3792,6 +4306,7 @@ exams =
   , taitPreSolo
   , taitPreAreaSolo
   , taitPreRPL
+  , airborneAviationPreSolo
   , airborneAviationAreaSolo
   ]
 
