07924dd6fa3bcd32f0426537f05b106bff9aa3c1
Tony Morris <tmorris@tmorris.net>
2016-03-05 16:42:23 +1000

    airborne-aviation.com.au Meteorology

--- practice-exams.hs
+++ practice-exams.hs
@@ -4296,6 +4296,137 @@ airborneAviationAreaSolo =
         ]                                               
     ]
 
+
+airborneAviationMeteorology ::
+  Exam
+airborneAviationMeteorology =
+  Exam
+    "airborne-aviation.com.au Meteorology"
+    Nothing
+    (Just "MET01")
+    [
+      "A wind in a terminal area forecast expressed as VRB30G45 would:" ~>
+      Multichoice
+        [
+          "Always be associated with closely spaced isobars around a trough"
+        , "Be improbable, since variable wind directions are only associated with light winds and high pressure systems"
+        ]
+
+        "Probably be associated with a thunderstorm"
+
+        [
+          "Always be associated with closely spaced isobars around a ridge"
+        ]
+    , "The validity of a METAR is:" ~>
+      Multichoice
+        [
+          "3 hours"
+        , "6 hours"
+        , "30 hours"
+        ]
+
+        "Not applicable"
+
+        [
+        ]
+    , "The wind direction is given in degrees:" ~>
+      Multichoice
+        [
+          "Magnetic in both area and aerodrome forecasts"
+        , "Magnetic in an area forecast, and true in an aerodrome forecast"
+        ]
+
+        "True in both area and aerodrome forecasts"
+
+        [
+          "True in an area forecast, and magnetic in an aerodrome forecast"
+        ]
+    , "In a METAR for YAAA, the figures quoted immediately before the QNH read 9/8, and at YBBB they read 10/8. This means that, at place YAAA, the dry bulb is 9C and the:" ~>
+      Multichoice
+        [
+        ]
+
+        "Dew point is 8C and fog is closer to forming at YAAA than YBBB"
+
+        [
+          "Wet bulb is 8C and fog is closer to forming at YAAA than YBBB"
+        , "Dew point is 8C and fog is closer to forming at YBBB than YAAA"
+        , "Wet bulb is 8C and fog is closer to forming at YBBB than YAAA"
+        ]
+    , "A TAF includes the following:\n\n> INTER 0710/0714 2000 -SHSN\n\nThis forecasts, between the times indicated:" ~>
+      Multichoice
+        [
+          "Showers and snow of expected duration 30 to 60 minutes"
+        , "Showers of snow of expected duration 30 to 60 minutes"
+        , "Showers and snow of expected duration less than 30 minutes"
+        ]
+
+        "Showers of snow of expected duration less than 30 minutes"
+
+        [
+        ]
+    , "The validity period of an aerodrome forecast is from 0000Z on the 1st to 0600Z on the 2nd. In the forecast this is written:" ~>
+      Multichoice
+        [
+          "0100/0206"
+        , "010006"
+        , "0100/06"
+        ]
+
+        "01000206"
+
+        [
+        ]
+    , "In a TAF, the term FU VV030 means:" ~>
+      Multichoice
+        [
+          "Smoke and a visibility of 3,000 metres"
+        ]
+
+        "Smoke and a vertical visibility of 3,000 feet"
+
+        [
+          "Fumes and a visibility of 3.000 metres"
+        , "Fumes and a vertical visibility of 3,000 feet"
+        ]
+    , "Observed intermediate QNH values are rounded:" ~>
+      Multichoice
+        [
+          "Down so that the altimeter reading is from a pressure level slightly lower than MSL"
+        ]
+
+        "Down so that the altimeter reading is from a pressure level slightly higher than MSL"
+
+        [
+          "Up so that the altimeter reading is from a pressure level slightly lower than MSL"
+        , "Up so that the altimeter reading is from a pressure level slightly higher than MSL"
+        ]
+    , "When cruising below the transition layer (10,000') you can set a local QNH of a station along the route provided it is within:" ~>
+      Multichoice
+        [
+          "10NM of the aircraft"
+        , "50NM of the aircraft"
+        ]
+
+        "100NM of the aircraft"
+
+        [
+          "200NM of the aircraft"
+        ]
+    , "A TAF commences and finishes as follows:\n\n> TAF YCOM 070635Z 0708/0720 ... Q 1020 1021 1019 1018\n\nThe forecast QNH for time 071320 is:" ~>
+      Multichoice
+        [
+          "1020"
+        , "1021"
+        ]
+
+        "1019"
+
+        [
+          "1018"
+        ]
+    ]
+
 exams ::
   [Exam]
 exams =
@@ -4308,6 +4439,7 @@ exams =
   , taitPreRPL
   , airborneAviationPreSolo
   , airborneAviationAreaSolo
+  , airborneAviationMeteorology
   ]
 
 main ::
