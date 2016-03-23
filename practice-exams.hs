8eb386a99c53f485668acf9739e9c5702a141072
Tony Morris <tmorris@tmorris.net>
2016-03-23 21:24:56 +1000

    Bob Tait RPL part 13 meteorology questions

--- practice-exams.hs
+++ practice-exams.hs
@@ -4415,6 +4415,172 @@ airborneAviationMeteorology =
         ]
     ]
 
+taitPart13Meteorology ::
+  Exam
+taitPart13Meteorology =
+  Exam
+    "Bob Tait RPL Study Guide"
+    (Just "A study guide for the Recreational Pilot Licence")
+    (Just "Part 13 Meteorology")
+    [
+      "Which conditions are most likely to produce radiation fog?" ~>
+      Multichoice
+        [              
+          "thin overcast with no wind"
+        , "broken cloud with no wind"
+        ]
+
+        "nil cloud and light wind"
+
+        [
+          "scattered cumulus with light wind"
+        ]
+    , "During what stage of a thunderstorm would lightning be most frequent?" ~>
+      Multichoice
+        [
+          "before any precipitation"
+        , "during the most rapid growth"            
+        ]
+
+        "at the mature stage"
+
+        [
+          "during the dissipating stage"
+        ]
+    , "The lifting of fog may be caused by" ~>
+      Multichoice
+        [              
+          "a fall of temperature"
+        , "the formation of upper cloud"
+        ]
+
+        "an increase in wind strength"
+
+        [
+          "an increase in humidity"
+        ]
+    , "Which phenomena in a thunderstorm causes low level wind shear at some distance away from the cell?" ~>
+      Multichoice
+        [              
+          "updraft"
+        ]
+
+        "downdraft"
+
+        [
+          "hail"
+        , "turbulence"
+        ]
+    , "Where are tornadoes most likely to be encountered?" ~>
+      Multichoice
+        [              
+          "over hot desert country"
+        , "with mountain waves"
+        , "strong low pressure systems"
+        ]
+
+        "wide spread severe thunderstorm activity"
+
+        [
+        ]
+    , "Which process is involved in the formation of hoar frost?" ~>
+      Multichoice
+        [              
+          "accumulation of ice crystals suspended in the atmosphere"
+        , "liquid water freezing on impact with the aircraft surface"
+        ]
+
+        "deposition of ice directly from the water vapour mixed with the air"
+
+        [
+          "freezing of large supercooled water droplets"
+        ]
+    , "The condition most likely to produce advection fog is" ~>
+      Multichoice
+        [              
+          "cold air passing over a cold surface"
+        , "warm air passing over a warm surface"
+        , "cold air passing over a warm surface"
+        ]
+
+        "warm air passing over a cold surface"
+
+        [
+        ]
+    , "One of the prerequisites for the formation of a thunderstorm is" ~>
+      Multichoice
+        [              
+        ]
+
+        "the presence of moist air through a considerable depth of the atmosphere"
+
+        [
+          "a rapid drop in surface temperature with an increase in wind strength"
+        , "a gradual increase in cloud cover above 20000ft"
+        , "a cold isothermal layer through a considerable depth of the atmosphere"
+        ]
+    , "Advection fog forms when" ~>
+      Multichoice
+        [              
+          "cold air passing over a cold surface"
+        , "warm air passing over a warm surface"
+        , "cold air passing over a warm surface"
+        ]
+
+        "warm air passing over a cold surface"
+
+        [
+        ]
+    , "Dust storms are most likely to produce which of the following hazards?" ~>
+      Multichoice
+        [              
+          "severe visibility restriction in a localised area below 10000ft"
+        , "severe visibility restriction over a widespread area below 10000ft"
+        , "severe visibility restriction over a localised area to heights above 10000ft"
+        ]
+
+        "severe visibility restriction over a widespread area to heights above 10000ft"
+
+        [
+        ]
+    , "The most dangerous effect of a mature thunderstorm is" ~>
+      Multichoice
+        [              
+        ]
+
+        "turbulence"
+
+        [
+          "lightning"
+        , "hail"
+        , "icing"
+        ]
+    , "Slant visibility in fog causes" ~>
+      Multichoice
+        [              
+          "reduced visibility towards the sun"
+        ]
+
+        "the runway is visible from 'over the top' but not in the circuit area or on final"
+
+        [
+          "better visibility looking up than down"
+        , "runway visual range is less than meteorological visibility"
+        ]
+    , "The runway at a coastal aerodrome runs parallel to the coast. When would the crosswind component be likely to be strongest?" ~>
+      Multichoice
+        [              
+          "early in the morning"
+        , "late at night"
+        ]
+
+        "in the early afternoon"
+
+        [
+          "at sunset"
+        ]
+    ]
+
 exams ::
   [Exam]
 exams =
@@ -4425,6 +4591,7 @@ exams =
   , taitPreSolo
   , taitPreAreaSolo
   , taitPreRPL
+  , taitPart13Meteorology
   , airborneAviationPreSolo
   , airborneAviationAreaSolo
   , airborneAviationMeteorology
