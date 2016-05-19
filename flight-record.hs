bd71d2d26c35f98780849f5a179ea7b95e554033
Tony Morris <tmorris@tmorris.net>
2016-05-19 14:40:23 +1000

    flight record

--- flight-record.hs
+++ flight-record.hs
@@ -1147,6 +1147,109 @@ flightlogentries =
               (Just "Live stream")
               Bambuser
           ]
+        )             , FlightLogEntry
+        "Practice Forced Landings"
+        "Practice Forced Landings"
+        "20160509"
+        vhafr
+        (
+          Hours
+            1
+            0
+        )
+        dual
+        ybaf2ybaf
+        Day
+        "David Schofield"
+        (
+          TrackLogs
+            [
+              TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/20160509-vh-afr.gpx"
+              Nothing
+              Gpx      
+            , TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/png/20160509-vh-afr.png"
+              Nothing
+              (ImageLog Png)                    
+            ]
+        )
+        (
+          Visualisations
+          [
+            Visualisation
+              "http://doarama.com/view/741813"
+              Nothing
+              Doarama
+          ]
+        )
+        (
+          Images
+          [
+          ]
+        )
+        (
+          Videos
+          [
+            Video
+              "https://www.youtube.com/watch?v=o1dx3hZuov0"
+              (Just "Forward camera")
+              YouTube
+          , Video
+              "https://bambuser.com/v/6250595"
+              (Just "Live stream")
+              Bambuser
+          ]
+        )          
+      , FlightLogEntry
+        "Steep Turns"
+        "Steep Turns"
+        "20160519"
+        vhvvo
+        (
+          Hours
+            1
+            1
+        )
+        dual
+        ybaf2ybaf
+        Day
+        "Ryan Low"
+        (
+          TrackLogs
+            [
+              TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/20160519-vh-vvo.gpx"
+              Nothing
+              Gpx      
+            , TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/png/20160519-vh-vvo.png"
+              Nothing
+              (ImageLog Png)                    
+            ]
+        )
+        (
+          Visualisations
+          [
+            Visualisation
+              "http://doarama.com/view/756397"
+              Nothing
+              Doarama
+          ]
+        )
+        (
+          Images
+          [
+          ]
+        )
+        (
+          Videos
+          [
+            Video
+              "https://bambuser.com/v/6267124"
+              (Just "Live stream")
+              Bambuser
+          ]
         )                                             
     ]
 
