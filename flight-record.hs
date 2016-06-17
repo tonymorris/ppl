1e1bcd1392aac49e85e28a1bd01a5e417d511d37
Tony Morris <tmorris@tmorris.net>
2016-06-17 13:57:42 +1000

    20160617 VH-VVO

--- flight-record.hs
+++ flight-record.hs
@@ -1422,6 +1422,55 @@ flightlogentries =
               Bambuser
           ] 
         )                                           
+      , FlightLogEntry
+        "Area Solo Check"
+        "20160617"
+        vhvvo
+        (
+          Hours
+            1
+            5
+        )
+        solo
+        ybaf2ybaf
+        Day
+        "Tony Morris"
+        (
+          TrackLogs
+            [
+              TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/20160617-vh-vvo.gpx"
+              Nothing
+              Gpx      
+            , TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/png/20160617-vh-vvo.png"
+              Nothing
+              (ImageLog Png)                    
+            ]
+        )
+        (
+          Visualisations
+          [
+            Visualisation
+              "http://doarama.com/view/801102"
+              Nothing
+              (Doarama "6rabNGE")
+          ]
+        )
+        (
+          Images
+          [
+          ]
+        )
+        ( 
+          Videos
+          [
+            Video
+              "6320415"
+              (Just "Live stream")
+              Bambuser
+          ] 
+        )                                           
     ]
 
 flightlog ::
