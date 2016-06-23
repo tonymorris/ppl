b7660c23785cb96b5889200cdb28c70c3445fc18
Tony Morris <tmorris@tmorris.net>
2016-06-23 14:59:42 +1000

    add Flight One FROL exam

--- practice-exams.hs
+++ practice-exams.hs
@@ -5091,6 +5091,497 @@ frol =
         "two thousand Oscar Romeo Echo [transponder to 2000]"
     ]
 
+flightOneFROL ::
+  Exam
+flightOneFROL =
+  Exam
+    "Flight One paper FROL exam"
+    (Just "Flight One paper FROL exam")
+    Nothing
+    [
+      "As a VFR aircraft, airborne and tracking for a Class C Control Zone to transit, you should:" ~>
+      Multichoice
+        [
+        ]
+
+        "Call the local Centre/Radar Frequency for a Squawk Code and Clearance"
+
+        [
+          "Call the associated Military Approach (APP) Frequency for a Squawk Code and Clearance"
+        , "Call the associated Military Airways Clearance Delivery (ACD) frequency for a Squawk Code and Clearance"
+        , "Call the associated Aerodrome Tower (TWR) frequency for a Squawk Code and Clearance"
+        ]
+    , "Attenuation can be best described as:" ~>
+      Multichoice
+        [
+          "A button pressed on the audio panel which attenuates the volume so you can listen to your ipod and monitor the COMS at the same time"
+        ]
+
+        "The absorption and scattering of some radio waves as a result of water vapour, cloud, dust, etc."
+
+        [
+          "A decrease in the power of radio waves returning to a transceiver as a result of great distance travelled"
+        , "A decrease in the power of radio waves arriving at a transceiver as a result of great distance travelled"
+        ]
+    , "If you switch to another ATC unit, wait to find no other transmissions and make your call to find the response: TWO STATIONS IN TOGETHER ON BRISBANE CENTRE. This means:" ~>
+      Multichoice
+        [
+          "Your transmission was over transmitted by someone else and you should await their call then make yours"
+        , "Your transmission was over transmitted by someone else and you should make your call again quickly before the jump on your transmission a second time"
+        ]
+
+        "Your transmission was made simultaneously with another station and you should wait for the other station to make a call unless yours is of an urgent or distress manner"
+
+        [
+          "Your transmission was made simultaneously with another station and you should make your call again quickly, regardless of the nature"
+        ]
+    , "A cable antenna extending from one extremity of an aircraft to another under a degree of tension is most likely to be for which of the following?" ~>
+      Multichoice
+        [
+          "The ADF"
+        ]
+
+        "The HF Radio"
+
+        [
+          "The VHF COM"
+        , "The VHF NAV"
+        ]
+    , "The NDB/ADF system relies primarily on which of the following types of wave propagation?" ~>
+      Multichoice
+        [
+          "Sky waves"
+        , "Direct waves"
+        ]
+
+        "Ground waves"
+
+        [
+          "None of the above"
+        ]
+    , "Once you have established two-way COMS with a ATC Unit and you have been **IDENTIFIED**, this means:" ~>
+      Multichoice
+        [
+          "You are under radar control"
+        , "Your transponder is on the correct code and is periodically interrogated by Secondary Surveillance Radar"
+        , "You are given ATC guided separation from IFR Traffic"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "Which of the following is/are some or all of the minimum requirements to hold a Radiotelephone Operator Licence?" ~>
+      Multichoice
+        [
+          "Pass a written test/examination"
+        , "Pass a PPL *(should be RPL)* flight test or other suitable practical test"
+        , "Be at least 16 years of age or older"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "After receiving a report of: READING YOU FIFES. From ATC, you understand that:" ~>
+      Multichoice
+        [
+        ]
+
+        "On a scale of one to five for clarity and strength, you have five for each"
+
+        [
+          "Your radio is only working at half strength and clarity"
+        , "Your radio is clear but signal is faint"
+        , "Carrier wave only"
+        ]
+    , "VHF radio waves propagate as:" ~>
+      Multichoice
+        [
+          "Sky waves"
+        ]
+
+        "Direct waves"
+
+        [
+          "Ground waves"
+        , "None of the above"
+        ]
+    , "The Automatic Terminal Information Service (ATIS) is usually broadcast on an aerodrome's:" ~>
+      Multichoice
+        [
+          "Discrete ATIS only frequency where available"
+        , "VHF NAV frequency where available"
+        , "NDB frequency where available"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "Which of the following styles of headphones and microphone combination Jack might you possibly find in a general aviation aircraft?" ~>
+      Multichoice
+        [
+        ]
+
+        "Two Jacks: one headphone one microphone with straight cables"
+
+        [
+          "Two Jacks: one headphone one microphone with coiled cables"
+        , "1 Jack: All through one straight cable"
+        , "None of the above"
+        ]
+    , "Approaching a Class C control boundary you plan to request clearance having not made two-way COMS with the ATC unit during this sector. The most correct example of an approaching radio call is:" ~>
+      Multichoice
+        [
+          "Brisbane Centre, Alpha Bravo Charlie. (to gain two way COMS first before your request)"
+        ]
+
+        "Brisbane Centre, Alpha Bravo Charlie Cessna 172 is over head Maleny, 4500 feet, tracking for Archerfield Request clearance"
+
+        [
+          "Brisbane Centre, Alpha Bravo Charlie Cessna 172 is over head Maleny, 4500 feet, tracking for Archerfield Request Code"
+        , "Brisbane Centre, Alpha Bravo Charlie, request when ready. (to gain two way COMS first before your request)"
+        ]
+    , "Which of the following is not an error associated with VHF NAV Ground stations?" ~>
+      Multichoice
+        [
+          "Site Error"
+        ]
+
+        "Terrain effect"
+
+        [
+          "Aggregate Error"
+        , "Scalloping"
+        ]
+    , "The Automatic Terminal Information Service (ATIS) is usually broadcast on an aerodrome's:" ~>
+      Multichoice
+        [
+          "Discrete ATIS only frequency where available"
+        , "HF frequency where available"
+        , "VHF NAV frequency where available"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "On the radio control panel, is it possible to set up the avionics so that multiple radios are receiving at once?" ~>
+      Multichoice
+        [
+          "Yes - but only if it is a Garmin Stack"
+        ]
+
+        "Yes - select the COM and NAV units desired to be receiving and tune correct frequency"
+
+        [
+          "No - no brand is capable of this at this stage"
+        , "No - you can only select one COM and one NAV at a time"
+        ]
+    , "A departure report for a VFR aircraft is required when operating at a:" ~>
+      Multichoice
+        [
+          "Class C Aerodrome"
+        , "Class D Aerodrome"
+        , "Class G Aerodrome"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "When requested by an ATC unit ALPHA BRAVO CHARLIE, BRISBANE CENTRE, RADIO CHECK HOW DO YOU READ? To give a report of very good clarity and strength, you would reply:" ~>
+      Multichoice
+        [
+        ]
+
+        "Reading you fifes"
+
+        [
+          "Reading you fine"
+        , "Reading you Nines"
+        , "None of the above"
+        ]
+    , "The NDB transmits typically within a range of:" ~>
+      Multichoice
+        [
+          "200 to 400"
+        ]
+
+        "200 to 430"
+
+        [
+          "100 to 400"
+        , "195 to 419"
+        ]
+    , "Where would you find the frequency for the Automatic Enroute Information Service (AERIS) that related to your current position?" ~>
+      Multichoice
+        [
+          "A World Aeronautical Chart (WAC)"
+        , "A Planning Chart Australia (PCA)"
+        ]
+
+        "A Visual Terminal Chart (VTC)"
+
+        [
+          "All of the above"
+        ]
+    , "" ~>
+      Multichoice
+        [
+          "The HF Radio relies primarily on which of the following types of wave propagation?"
+        ]
+
+        "Sky waves"
+
+        [
+          "Direct waves"
+        , "Ground waves"
+        , "None of the above"
+        ]
+    , "What is the purpose of an antenna?" ~>
+      Multichoice
+        [
+        ]
+
+        "To receive and transmit radio signals"
+
+        [
+          "To receive radio signals only"
+        , "To transmit radio signals only"
+        , "None of the above"
+        ]
+    , "An ATC unit replies to your request as: *Alpha Bravo Charlie, squawk code 3447, QNH 1016*. Your reply should be:" ~>
+      Multichoice
+        [
+          "Squawk code 3447, 1016 ALPHA BRAVO CHARLIE"
+        , "3447, QNH 1016 ALPHA BRAVO CHARLIE"
+        , "Squawk code 3447, QNH 1016 ALPHA BRAVO CHARLIE"
+        ]
+
+        "3447, 1016 ALPHA BRAVO CHARLIE"
+
+        [
+        ]
+    , "The use of flight radios is limited to" ~>
+      Multichoice
+        [
+          "Emergencies only"
+        , "Aviation related clearances, requests, cancellations"
+        , "Company communications"
+        ]
+
+        "All of the above"
+
+        [
+        ]
+    , "The squelch control on your radio can be best described as:" ~>
+      Multichoice
+        [
+          "A control to tune slim dusty and other country music during flight"
+        , "A control for individual headphone volume"
+        , "A control for overall system volume"
+        ]
+
+        "A control for pickup sensitivity at a microphone"
+
+        [
+        ]
+    , "When inside controlled airspace and transiting from one frequency zone to another, you are handed off to make two-way COMS with Brisbane Centre 121.2. Your first call on this frequency should be:" ~>
+      Multichoice
+        [
+          "Brisbane Centre 121.2 Alpha Bravo Charlie maintaining 4500"
+        , "Brisbane Centre 121.2 Alpha Bravo Charlie"
+        ]
+
+        "Brisbane Centre Alpha Bravo Charlie maintaining 4500"
+
+        [
+          "Brisbane Centre Alpha Bravo Charlie"
+        ]
+    , "As the holder of a FROL, you are authorised to:" ~>
+      Multichoice
+        [
+          "Operate a radiotelephone system that is installed in the aircraft only"
+        , "Operate a radiotelephone system that is carried but not hard wired in the aircraft only"
+        ]
+
+        "Operate a radiotelephone system that is installed or carried in the aircraft"
+
+        [
+          "Not allowed to use the system at all"
+        ]
+    , "A VHF NAV radio receives in a frequency band of:" ~>
+      Multichoice
+        [
+          "118.00 to 134.95"
+        , "117.00 to 133.95"
+        , "112.00 to 117.95"
+        ]
+
+        "108.00 to 116.95"
+
+        [
+        ]
+    , "Which of the following factors may reduce coverage or limit the useful range of a VHF transmitter?" ~>
+      Multichoice
+        [
+          "Thunderstorms"
+        , "Coastal Refraction"
+        ]
+
+        "Terrain separating transmitter and receiver"
+
+        [
+          "Scalloping"
+        ]
+    , "Which of the following is an accurate example of a distress call?" ~>
+      Multichoice
+        [
+        ]
+
+        "MAYDAY MAYDAY MAYDAY, Brisbane Radar, Alpha Bravo Charlie, Alpha Bravo Charlie, Alpha Bravo Charlie, Cessna 172, Engine Failure, Attempting Forced Landing near Logan Village, 2 POB."
+
+        [
+          "MAYDAY MAYDAY MAYDAY, Brisbane Radar, Alpha Bravo Charlie, Alpha Bravo Charlie, Alpha Bravo Charlie, Cessna 172, Engine Failure, Logan Village, 2 POB."
+        , "PAN PAN, PAN PAN, PAN PAN, Brisbane Radar, Alpha Bravo Charlie, Alpha Bravo Charlie, Alpha Bravo Charlie, Cessna 172, Engine Failure, Attempting Forced Landing near Logan Village, 2 POB."
+        , "PAN PAN, PAN PAN, PAN PAN, Brisbane Radar, Alpha Bravo Charlie, Alpha Bravo Charlie, Alpha Bravo Charlie, Cessna 172, Engine Failure, Attempting Forced Landing near Logan Village, 2 POB."
+        ]      
+    , "The memory verse of TUNE IDENTIFY TEST is to be carried out while using:" ~>
+      Multichoice
+        [
+          "VHF NAV"
+        , "VHF COM"
+        , "ADF"
+        ]
+
+        "VHF NAV and ADF"
+
+        [
+        ]          
+    , "Approaching the runway on final approach at a controlled aerodrome having conducted the appropriate COM Failure procedure, you look to the tower for confirmation the landing area is safe to land. You see a green light - to acknowledge receipt to the tower you should:" ~>
+      Multichoice
+        [
+        ]
+
+        "Flash the landing lights"
+
+        [
+          "Turn all lights off"
+        , "Turn all lights on"
+        , "Manouvre the aircraft in roll to 15deg AOB each way"
+        ]          
+    , "Company communications are limited to:" ~>
+      Multichoice
+        [
+          "Company discrete frequencies"
+        , "Pertinent operational information"
+        , "Discussions relating to company employees"
+        ]
+
+        "Company discrete frequencies and pertinent operational information"
+
+        [
+        ]          
+    , "When asked by an ATC unit to advise an estimate for a place of destination, you should express the following time as: June 24th 10:52am EST" ~>
+      Multichoice
+        [
+          "Estimate XYZ at 06241052"
+        , "Estimate XYZ at 06241052 ZULU"
+        , "Estimate XYZ at 1052 ZULU"
+        ]
+
+        "Estimate XYZ at 0052 ZULU"
+
+        [
+        ]          
+    , "Which of the following is a limitation of the NDB/ADF system?" ~>
+      Multichoice
+        [
+          "Scalloping"
+        , "Site Error"
+        ]
+
+        "Coastal Refraction"
+
+        [
+          "Line of sight"
+        ]          
+    , "What would be the transponder code to display in the event of a suspected COMS failure?" ~>
+      Multichoice
+        [
+          "7700"
+        ]
+
+        "7600"
+
+        [
+          "7500"
+        , "3000"
+        ]          
+    , "Some aircraft systems are installed with a coinciding Avionics Master Switch. What is the function of this switch?" ~>
+      Multichoice
+        [
+          "It is a way to cut power only from all radios"
+        , "It is a way to control which power source the radios operate from"
+        ]
+
+        "Controls power source of all radio equipment"
+
+        [
+          "Controls power to communications VHF radios only"
+        ]          
+    , "As a VFR aircraft, airborne and tracking for a Military Restricted Control Zone to transit, you should:" ~>
+      Multichoice
+        [
+          "Call the local Civilian Centre Frequency for a Squawk Code and Clearance"
+        , "Call the associated Military Approach (APP) Frequency for a Squawk Code and Clearance"
+        ]
+
+        "Call the associated Military Airways Clearance Delivery (ACD) frequency for a Squawk Code and Clearance"
+
+        [
+          "Call the associated Military Tower (TWR) frequency for a Squawk Code and Clearance"
+        ]          
+    , "WILCO means:" ~>
+      Multichoice
+        [
+          "Understood"
+        ]
+
+        "Understood and will comply with requirement"
+
+        [
+          "Looking for the traffic"
+        , "None of the above"
+        ]          
+    , "A VHF COM radio receives in a frequency band of:" ~>
+      Multichoice
+        [
+          "118.00 to 134.95"
+        ]
+
+        "117.00 to 133.95"
+
+        [
+          "109.00 to 117.95"
+        , "108.00 to 116.95"
+        ]          
+    , "123.6 as a frequency should be read as:" ~>
+      Multichoice
+        [
+          "ONE TWO THREE DAYCIMAL SIX"
+        ]
+
+        "WUN TOO TREE DAYCIMAL SIX"
+
+        [
+          "ONE TWO TREE DECIMAL SIX"
+        , "WUN TOO TREE DAYCIMAL SIX"
+        ]          
+    ]
+
 exams ::
   [Exam]
 exams =
