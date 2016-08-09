3d68e7ee2468bbf1830bb4a3fb2bd4b8890b00bb
Tony Morris <tmorris@tmorris.net>
2016-08-09 14:28:48 +1000

    20160809 VH-VVO

--- flight-record.hs
+++ flight-record.hs
@@ -1673,6 +1673,55 @@ entries =
           [ 
           ]          
         )       
+      , flight
+        "Area Solo Consolidation"
+        "20160809"
+        vhafr
+        ( 
+          Hours
+            1
+            2
+        )
+        dual
+        ybaf2ybaf
+        Day
+        "Tony Morris"
+        (
+          TrackLogs
+          [
+            TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/20160809-vh-vvo.gpx"
+              Nothing
+              Gpx
+          , TrackLog
+              "https://raw.githubusercontent.com/tonymorris/ppl/master/tracks/png/20160809-vh-vvo.png"
+              Nothing
+              (ImageLog Png)
+          ]
+        )
+        (
+          Visualisations
+          [
+            Visualisation
+              "http://doarama.com/view/882382"
+              Nothing
+              (Doarama "6jnWOa6")
+          ]
+        )
+        mempty
+        (
+          Videos
+          [
+            Video
+              "zKu7-WoWyXo"
+              (Just "Cabin live stream")
+              YouTube
+          , Video
+              "HMaIC3TJFXI"
+              (Just "Cabin live stream")
+              YouTube
+          ]          
+        )       
     ]
 
 logbook ::
