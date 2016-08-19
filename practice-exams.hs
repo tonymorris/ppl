0e448abe9e3f2b9f80d89b2369b06da29cbc4e10
Tony Morris <tmorris@tmorris.net>
2016-08-19 14:32:30 +1000

    airspeed indicator exam

--- practice-exams.hs
+++ practice-exams.hs
@@ -8642,6 +8642,43 @@ bobTaitChapterRevisionMeteorology =
         ]
     ]
 
+airspeedindicatorExam ::
+  Exam
+airspeedindicatorExam =
+  Exam
+    "Airspeed Indicator Exam"
+    (Just "A self-made exam")
+    Nothing
+    [
+      "What is the significance of the red band?" !-
+      DirectAnswer
+        "Vne (never exceed speed)."
+    , "Where is the never exceed speed indicated?" !-
+      DirectAnswer
+        "End of yellow band, start of red band."
+    , "What is the significance of the yellow band?" !-
+      DirectAnswer
+        "Vno (maximum structural cruise) to Vne (never exceed)."
+    , "What is the significance of the white band?" !-
+      DirectAnswer
+        "Vso (stall speed with full flaps) to Vfe (maximum speed with flaps extended)."
+    , "Where is the stall speed with full flaps indicated?" !-
+      DirectAnswer
+        "Start of white band."
+    , "Where is the maximum speed with flaps extended indicated?" !-
+      DirectAnswer
+        "End of white band."
+    , "What is the significance of the green band?" !-
+      DirectAnswer
+        "Vs1 (stall speed with no flap, no gear) to Vno (maximum structural cruise)."
+    , "Where is the stall speed with no flap indicated?" !-
+      DirectAnswer
+        "Start of green band."
+    , "Where is the maximum structural cruise indicated?" !-
+      DirectAnswer
+        "End of green band, start of yellow band."
+    ]
+
 bobTaitChapterRevision ::
   [Exam]
 bobTaitChapterRevision =
@@ -8667,6 +8704,7 @@ bobTaitChapterRevision =
   , bobTaitChapterRevisionHPTEM
   , bobTaitChapterRevisionAirLaw
   , bobTaitChapterRevisionMeteorology
+  , airspeedindicatorExam
   ]
 
 exams ::
