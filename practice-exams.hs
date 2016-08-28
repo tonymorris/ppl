c407078194ce67f5af27cf4d2a9304b3dcf025e4
Tony Morris <tmorris@tmorris.net>
2016-08-29 07:45:56 +1000

    exams

--- practice-exams.hs
+++ practice-exams.hs
@@ -8737,36 +8737,15 @@ airspeedsExam =
         "Vs1"
     ]
 
-form61_1486_1495 ::
+c172airspeeds ::
   Exam
-form61_1486_1495 =
+c172airspeeds =
   Exam
-    "Form 61-1486 and 61-1495"
-    (Just "A self-made exam")
+    ""
+    Nothing
     Nothing
     [
-      "What are the privileges and limitations of the recreational pilot licence with aeroplane category rating?" !-
-      DirectAnswer
-        "* can carry passengers if at least 3 take-offs and landings within previous 90 days *[CASR61.395]*\n* pilot holds and carries *[CASR61.420(b)]* class 1 or 2 medical certificate or RAMCP under conditions in subparagraph 2 *[CASR61.405]*\n* pilot must carry documents; licence, medical certificate, maintenance release, pilot operating handbook\n* must have English proficiency assessment *[CASR61.422]*\n* pilot only registered aircraft *[CASR61.425]*\n* airspace within 25nm of departure aerodrome, within flight training area and direct between departure aerodrome and flight training area *[CASR61.427]*\n* single-engine aircraft, MTOW <= 1500kg, day VFR, private operation or flight training *[CASR61.460]*"
-    , "What are the drug and alcohol regulations?" !-
-      DirectAnswer
-        "* 8 hours from consumption of alcohol to departure\n* not intoxicated; 0.02 grams per 210 litres of breath\n* crew will not consume while on board\n* consult DAME regarding use of drugs"
-    , "What are the VFR aircraft instrument requirements?" !-
-      DirectAnswer
-        "* *CAO20.18(10)* and *CAO20.18(Appendix 1)*\n* airspeed indicator\n* altimeter\n* direct or remote magnetic compass\n* timepiece with hours, minutes, seconds"
-    , "What are the emergency equipment requirements?" !-
-      DirectAnswer
-        "* life jackets when over water and out of glide *CAO20.11(5.1)*\n* sufficient life raft(s) when minimum of (30 minutes cruise) and (100nm) *CAO20.11(5.2.1)*\n* if life raft required, 1 ELT and pyro distress signals *CAO20.11(6.1)*\n* if more than one life raft required, then >= 2 (approved ELT under reg 252A) transmitters 121.5MHz and 243MHz and stowed ready for use *CAO20.11(6.1)*\n* single-engine, over water, not equipped with radio or incapable of air-to-ground radio, not required to carry a life raft, shall carry ELT (121.5MHz and 243MHz approved under reg 252A) *CAO20.11(6.2)*"
-    , "What emergency procedures must the passengers be briefed on *CAO20.11(14)*?" !-
-      DirectAnswer
-        "* smoking requirements *CAO20.11(14.1.1)*\n* use of seat belts *CAO20.11(14.1.1)*\n* location of emergency exits *CAO20.11(14.1.1)*\n* use of oxygen where applicable *CAO20.11(14.1.1)*\n* use of floatation devices where applicable *CAO20.11(14.1.1)*\n* stowage of luggage *CAO20.11(14.1.1)*\n* onboard survival equipment *CAO20.11(14.1.1)*"
-    , "What are the fuel and oil requirements for flight?" !-
-      DirectAnswer
-        "* PiC must take steps to ensure sufficient fuel and oil *[CAR1988(234)]*\n* Fixed fuel reserve for VFR, aeroplane, piston-engine: 45 minutes *[CAAP 234-1(1)]*\n* 5-8 quarts of oil *Cessna 172 PoH Section 8 CAPACITY OF ENGINE SUMP*"
-    , "What are cargo and passenger loading requirements?" !-
-      DirectAnswer
-        "* cargo on or above floor shall be restrained *CAO20.16.2(3)*\n* cargo shall not obstruct flight controls *CAO20.16.2(4.1)*, emergency exits *CAO20.16.2(4.2)*\n* cargo on a passenger seat shall evenly distribute weight, not exceeding 77kg *CAO20.16.2(5.1)* and restrained *CAO20.16.2(5.2)*\n* cargo on unoccupied control seat shall not exceed 77kg *CAO20.16.2(6.2)*, restrained *CAO20.16.2(6.4)*, flight controls removed if easy *CAO20.16.2(6.3.1)*, not interfere with aircraft operation *CAO20.16.2(6.3)*\n* seat belts during take-off, landing, < 1000ft AGL, turbulence *CAO20.16.3(4.1)*\n* one pilot crew wearing seat belt at all times *CAO20.16.3(4.2)*\n* seats upright during take-off and landing *CAO20.16.3(5.1)*\n* passenger in control seat must be given instruction, no interfere with flight controls *CAO20.16.3(11.1)*\n* two infants (<=3 years of age) may be carried on one seat with total weight <= 77kg *CAO20.16.3(13.1)*"
-    , "What is the Vne for C172R?" !-
+      "What is the Vne for C172R?" !-
       DirectAnswer
         "163KIAS"
     , "What is the Vno for C172R?" !-
@@ -8886,6 +8865,37 @@ form61_1486_1495 =
     , "What is the maximum crosswind for C172S?" !-
       DirectAnswer
         "15 knots"
+    ]
+
+form61_1486_1495 ::
+  Exam
+form61_1486_1495 =
+  Exam
+    "Form 61-1486 and 61-1495"
+    (Just "A self-made exam")
+    Nothing
+    [
+      "What are the privileges and limitations of the recreational pilot licence with aeroplane category rating?" !-
+      DirectAnswer
+        "* can carry passengers if at least 3 take-offs and landings within previous 90 days *[CASR61.395]*\n* pilot holds and carries *[CASR61.420(b)]* class 1 or 2 medical certificate or RAMCP under conditions in subparagraph 2 *[CASR61.405]*\n* pilot must carry documents; licence, medical certificate, maintenance release, pilot operating handbook\n* must have English proficiency assessment *[CASR61.422]*\n* pilot only registered aircraft *[CASR61.425]*\n* airspace within 25nm of departure aerodrome, within flight training area and direct between departure aerodrome and flight training area *[CASR61.427]*\n* single-engine aircraft, MTOW <= 1500kg, day VFR, private operation or flight training *[CASR61.460]*"
+    , "What are the drug and alcohol regulations?" !-
+      DirectAnswer
+        "* 8 hours from consumption of alcohol to departure\n* not intoxicated; 0.02 grams per 210 litres of breath\n* crew will not consume while on board\n* consult DAME regarding use of drugs"
+    , "What are the VFR aircraft instrument requirements?" !-
+      DirectAnswer
+        "* *CAO20.18(10)* and *CAO20.18(Appendix 1)*\n* airspeed indicator\n* altimeter\n* direct or remote magnetic compass\n* timepiece with hours, minutes, seconds"
+    , "What are the emergency equipment requirements?" !-
+      DirectAnswer
+        "* life jackets when over water and out of glide *CAO20.11(5.1)*\n* sufficient life raft(s) when minimum of (30 minutes cruise) and (100nm) *CAO20.11(5.2.1)*\n* if life raft required, 1 ELT and pyro distress signals *CAO20.11(6.1)*\n* if more than one life raft required, then >= 2 (approved ELT under reg 252A) transmitters 121.5MHz and 243MHz and stowed ready for use *CAO20.11(6.1)*\n* single-engine, over water, not equipped with radio or incapable of air-to-ground radio, not required to carry a life raft, shall carry ELT (121.5MHz and 243MHz approved under reg 252A) *CAO20.11(6.2)*"
+    , "What emergency procedures must the passengers be briefed on *CAO20.11(14)*?" !-
+      DirectAnswer
+        "* smoking requirements *CAO20.11(14.1.1)*\n* use of seat belts *CAO20.11(14.1.1)*\n* location of emergency exits *CAO20.11(14.1.1)*\n* use of oxygen where applicable *CAO20.11(14.1.1)*\n* use of floatation devices where applicable *CAO20.11(14.1.1)*\n* stowage of luggage *CAO20.11(14.1.1)*\n* onboard survival equipment *CAO20.11(14.1.1)*"
+    , "What are the fuel and oil requirements for flight?" !-
+      DirectAnswer
+        "* PiC must take steps to ensure sufficient fuel and oil *[CAR1988(234)]*\n* Fixed fuel reserve for VFR, aeroplane, piston-engine: 45 minutes *[CAAP 234-1(1)]*\n* 5-8 quarts of oil *Cessna 172 PoH Section 8 CAPACITY OF ENGINE SUMP*"
+    , "What are cargo and passenger loading requirements?" !-
+      DirectAnswer
+        "* cargo on or above floor shall be restrained *CAO20.16.2(3)*\n* cargo shall not obstruct flight controls *CAO20.16.2(4.1)*, emergency exits *CAO20.16.2(4.2)*\n* cargo on a passenger seat shall evenly distribute weight, not exceeding 77kg *CAO20.16.2(5.1)* and restrained *CAO20.16.2(5.2)*\n* cargo on unoccupied control seat shall not exceed 77kg *CAO20.16.2(6.2)*, restrained *CAO20.16.2(6.4)*, flight controls removed if easy *CAO20.16.2(6.3.1)*, not interfere with aircraft operation *CAO20.16.2(6.3)*\n* seat belts during take-off, landing, < 1000ft AGL, turbulence *CAO20.16.3(4.1)*\n* one pilot crew wearing seat belt at all times *CAO20.16.3(4.2)*\n* seats upright during take-off and landing *CAO20.16.3(5.1)*\n* passenger in control seat must be given instruction, no interfere with flight controls *CAO20.16.3(11.1)*\n* two infants (<=3 years of age) may be carried on one seat with total weight <= 77kg *CAO20.16.3(13.1)*"          
     , "What are the privileges and limitations of the single engine aeroplane class rating?" !-
       DirectAnswer
         "* pilot licence must demonstrate competency of part 61 MoS *[CASR61.400]*\n* flight review every 24 months, within 3 months of expiry, and valid to the end of that month *[CASR61.745]*"
@@ -8948,8 +8958,11 @@ exams =
   [
     instruments
   ] ++
-  bobTaitChapterRevision
-
+  bobTaitChapterRevision ++
+  [
+    c172airspeeds
+  , form61_1486_1495
+  ]
 
 main ::
   IO ()
