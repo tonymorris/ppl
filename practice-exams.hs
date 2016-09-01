d0f27fe93cc7d5995ca8993d5662a423811ca56f
Tony Morris <tmorris@tmorris.net>
2016-09-01 17:27:03 +1000

    fix practice exams

--- practice-exams.hs
+++ practice-exams.hs
@@ -8,24 +8,31 @@ import System.Environment
 
 data Answer a =
   DirectAnswer a
+  | DirectListAnswer [a]
   | Multichoice [a] a [a]
   deriving (Eq, Ord, Show)
 
 instance Functor Answer where
   fmap f (DirectAnswer a) =
     DirectAnswer (f a)
+  fmap f (DirectListAnswer a) =
+    DirectListAnswer (fmap f a)
   fmap f (Multichoice l x r) =
     Multichoice (fmap f l) (f x) (fmap f r)
 
 instance Foldable Answer where
   foldr f z (DirectAnswer a) =
     f a z
+  foldr f z (DirectListAnswer a) =
+    foldr f z a
   foldr f z (Multichoice l x r) =
     foldr f (f x (foldr f z r)) l
 
 instance Traversable Answer where
   traverse f (DirectAnswer a) =
     DirectAnswer <$> f a
+  traverse f (DirectListAnswer a) =
+    DirectListAnswer <$> traverse f a
   traverse f (Multichoice l x r) =
     Multichoice <$> traverse f l <*> f x <*> traverse f r
 
@@ -34,6 +41,8 @@ choiceList ::
   -> [a]
 choiceList (DirectAnswer a) =
   [a]
+choiceList (DirectListAnswer a) =
+  a
 choiceList (Multichoice l x r) =
   l <> (x : r)
 
@@ -43,6 +52,8 @@ onFocus ::
   -> Answer a
 onFocus f (DirectAnswer a) =
   DirectAnswer (f a)
+onFocus f (DirectListAnswer a) =
+  DirectListAnswer (fmap f a)
 onFocus f (Multichoice l x r) =
   Multichoice l (f x) r
 
@@ -55,11 +66,13 @@ number q =
 
 focus ::
   Answer a
-  -> a
+  -> [a]
 focus (DirectAnswer a) =
+  [a]
+focus (DirectListAnswer a) =
   a
 focus (Multichoice _ x _) =
-  x
+  [x]
 
 data Question a =
   Question
@@ -124,6 +137,8 @@ markdownExam (Exam t1 t2 t3 qs) =
                   zipWith (\n c -> "  " <> show n <> ". " <> c) [1..] (l ++ ("**" ++ z ++ "**") : r) >>= (<> "\n")
                 DirectAnswer a ->
                   "\n  **" ++ a ++ "**\n"
+                DirectListAnswer a ->
+                  "\n" ++ ((\q -> "* **" ++ q ++ "**\n") =<< a) ++ "\n"
   in  intercalate "\n"
         [
           "# " <> t1 <> "\n" <> 
@@ -151,13 +166,19 @@ flashcardExam (Exam t1 t2 t3 qs) =
               case m of
                 Multichoice _ _ _ ->
                   let z = number m
+                      display :: (Char, String) -> String
                       display (c, s) = c : (". " <> s)
+                      displayN :: [(Char, String)] -> String
+                      displayN x = x >>= display
+                      rr = focus z
                   in  (intercalate "\n" . choiceList . fmap display $ z) <>
                       "\n\n" <>
-                      display (focus z) <>
+                      displayN (focus z) <>
                       "\n\n"
                 DirectAnswer a ->
                   "\n" ++ a ++ "\n"
+                DirectListAnswer a ->
+                  "\n" ++ intercalate "\n" a ++ "\n"
   in  intercalate "\n"
         [
           "# " <> t1 <> "\n" <> 
@@ -8876,35 +8897,89 @@ form61_1486_1495 =
     Nothing
     [
       "What are the privileges and limitations of the recreational pilot licence with aeroplane category rating?" !-
-      DirectAnswer
-        "* can carry passengers if at least 3 take-offs and landings within previous 90 days *[CASR61.395]*\n* pilot holds and carries *[CASR61.420(b)]* class 1 or 2 medical certificate or RAMCP under conditions in subparagraph 2 *[CASR61.405]*\n* pilot must carry documents; licence, medical certificate, maintenance release, pilot operating handbook\n* must have English proficiency assessment *[CASR61.422]*\n* pilot only registered aircraft *[CASR61.425]*\n* airspace within 25nm of departure aerodrome, within flight training area and direct between departure aerodrome and flight training area *[CASR61.427]*\n* single-engine aircraft, MTOW <= 1500kg, day VFR, private operation or flight training *[CASR61.460]*"
+      DirectListAnswer
+        [
+          "can carry passengers if at least 3 take-offs and landings within previous 90 days *[CASR61.395]*"
+        , "pilot holds and carries *[CASR61.420(b)]* class 1 or 2 medical certificate or RAMCP under conditions in subparagraph 2 *[CASR61.405]*"
+        , "pilot must carry documents; licence, medical certificate, maintenance release, pilot operating handbook"
+        , "must have English proficiency assessment *[CASR61.422]*"
+        , "pilot only registered aircraft *[CASR61.425]*"
+        , "airspace within 25nm of departure aerodrome, within flight training area and direct between departure aerodrome and flight training area *[CASR61.427]*"
+        , "single-engine aircraft, MTOW <= 1500kg, day VFR, private operation or flight training *[CASR61.460]*"
+        ]
     , "What are the drug and alcohol regulations?" !-
-      DirectAnswer
-        "* 8 hours from consumption of alcohol to departure\n* not intoxicated; 0.02 grams per 210 litres of breath\n* crew will not consume while on board\n* consult DAME regarding use of drugs *[CASR99]*"
+      DirectListAnswer
+        [
+          "8 hours from consumption of alcohol to departure *[CASR99]*"
+        , "not intoxicated; 0.02 grams per 210 litres of breath *[CASR99]*"
+        , "crew will not consume while on board *[CASR99]*"
+        , "consult DAME regarding use of drugs *[CASR99]*"
+        ]
     , "What are the VFR aircraft instrument requirements?" !-
-      DirectAnswer
-        "* *CAO20.18(10)* and *CAO20.18(Appendix 1)*\n* airspeed indicator\n* altimeter\n* direct or remote magnetic compass\n* timepiece with hours, minutes, seconds"
+      DirectListAnswer
+        [
+          "airspeed indicator *[CAO20.18(10)* and *CAO20.18(Appendix 1)]*"
+        , "altimeter *[CAO20.18(10)* and *CAO20.18(Appendix 1)]*"
+        , "direct or remote magnetic compass *[CAO20.18(10)* and *CAO20.18(Appendix 1)]*"
+        , "timepiece with hours, minutes, seconds *[CAO20.18(10)* and *CAO20.18(Appendix 1)]*"
+        ]
     , "What are the emergency equipment requirements?" !-
-      DirectAnswer
-        "* life jackets when over water and out of glide *CAO20.11(5.1)*\n* sufficient life raft(s) when minimum of (30 minutes cruise) and (100nm) *CAO20.11(5.2.1)*\n* if life raft required, 1 ELT and pyro distress signals *CAO20.11(6.1)*\n* if more than one life raft required, then >= 2 (approved ELT under reg 252A) transmitters 121.5MHz and 243MHz and stowed ready for use *CAO20.11(6.1)*\n* single-engine, over water, not equipped with radio or incapable of air-to-ground radio, not required to carry a life raft, shall carry ELT (121.5MHz and 243MHz approved under reg 252A) *CAO20.11(6.2)*"
-    , "What emergency procedures must the passengers be briefed on *CAO20.11(14)*?" !-
-      DirectAnswer
-        "* smoking requirements *CAO20.11(14.1.1)*\n* use of seat belts *CAO20.11(14.1.1)*\n* location of emergency exits *CAO20.11(14.1.1)*\n* use of oxygen where applicable *CAO20.11(14.1.1)*\n* use of floatation devices where applicable *CAO20.11(14.1.1)*\n* stowage of luggage *CAO20.11(14.1.1)*\n* onboard survival equipment *CAO20.11(14.1.1)*"
+      DirectListAnswer
+        [
+          "life jackets when over water and out of glide *[CAO20.11(5.1)]*"
+        , "sufficient life raft(s) when minimum of (30 minutes cruise) and (100nm) *[CAO20.11(5.2.1)]*"
+        , "if life raft required, 1 ELT and pyro distress signals *[CAO20.11(6.1)]*"
+        , "if more than one life raft required, then >= 2 (approved ELT under reg 252A) transmitters 121.5MHz and 243MHz and stowed ready for use *[CAO20.11(6.1)]*"
+        , "single-engine, over water, not equipped with radio or incapable of air-to-ground radio, not required to carry a life raft, shall carry ELT (121.5MHz and 243MHz approved under reg 252A) *[CAO20.11(6.2)]*"
+        ]
+    , "What emergency procedures must the passengers be briefed on *[CAO20.11(14)]*?" !-
+      DirectListAnswer
+        [
+          "smoking requirements *[CAO20.11(14.1.1)]*"
+        , "use of seat belts *[CAO20.11(14.1.1)]*" 
+        , "location of emergency exits *[CAO20.11(14.1.1)]*"
+        , "use of oxygen where applicable *[CAO20.11(14.1.1)]*"
+        , "use of floatation devices where applicable *[CAO20.11(14.1.1)]*"
+        , "stowage of luggage *[CAO20.11(14.1.1)]*"
+        , "onboard survival equipment *[CAO20.11(14.1.1)]*"
+        ]
     , "What are the fuel and oil requirements for flight?" !-
-      DirectAnswer
-        "* PiC must take steps to ensure sufficient fuel and oil *[CAR1988(234)]*\n* Fixed fuel reserve for VFR, aeroplane, piston-engine: 45 minutes *[CAAP 234-1(1)]*\n* 5-8 quarts of oil *Cessna 172 PoH Section 8 CAPACITY OF ENGINE SUMP*"
+      DirectListAnswer
+        [
+          "PiC must take steps to ensure sufficient fuel and oil *[CAR1988(234)]*"
+        , "Fixed fuel reserve for VFR, aeroplane, piston-engine: 45 minutes *[CAAP 234-1(1)]*"
+        , "5-8 quarts of oil *[Cessna 172 PoH Section 8 CAPACITY OF ENGINE SUMP]*"
+        ]
     , "What are cargo and passenger loading requirements?" !-
-      DirectAnswer
-        "* cargo on or above floor shall be restrained *CAO20.16.2(3)*\n* cargo shall not obstruct flight controls *CAO20.16.2(4.1)*, emergency exits *CAO20.16.2(4.2)*\n* cargo on a passenger seat shall evenly distribute weight, not exceeding 77kg *CAO20.16.2(5.1)* and restrained *CAO20.16.2(5.2)*\n* cargo on unoccupied control seat shall not exceed 77kg *CAO20.16.2(6.2)*, restrained *CAO20.16.2(6.4)*, flight controls removed if easy *CAO20.16.2(6.3.1)*, not interfere with aircraft operation *CAO20.16.2(6.3)*\n* seat belts during take-off, landing, < 1000ft AGL, turbulence *CAO20.16.3(4.1)*\n* one pilot crew wearing seat belt at all times *CAO20.16.3(4.2)*\n* seats upright during take-off and landing *CAO20.16.3(5.1)*\n* passenger in control seat must be given instruction, no interfere with flight controls *CAO20.16.3(11.1)*\n* two infants (<=3 years of age) may be carried on one seat with total weight <= 77kg *CAO20.16.3(13.1)*"          
+      DirectListAnswer
+        [
+          "cargo on or above floor shall be restrained *[CAO20.16.2(3)]*"
+        , "cargo shall not obstruct flight controls *[CAO20.16.2(4.1)]*, emergency exits *[CAO20.16.2(4.2)]*"
+        , "cargo on a passenger seat shall evenly distribute weight, not exceeding 77kg *[CAO20.16.2(5.1)]* and restrained *[CAO20.16.2(5.2)]*"
+        , "cargo on unoccupied control seat shall not exceed 77kg *[CAO20.16.2(6.2)]*, restrained *[CAO20.16.2(6.4)]*, flight controls removed if easy *[CAO20.16.2(6.3.1)]*, not interfere with aircraft operation *[CAO20.16.2(6.3)]*"
+        , "seat belts during take-off, landing, < 1000ft AGL, turbulence *[CAO20.16.3(4.1)]*"
+        , "one pilot crew wearing seat belt at all times *[CAO20.16.3(4.2)]*"
+        , "seats upright during take-off and landing *[CAO20.16.3(5.1)]*"
+        , "passenger in control seat must be given instruction, no interfere with flight controls *[CAO20.16.3(11.1)]*"
+        , "two infants (<=3 years of age) may be carried on one seat with total weight <= 77kg *[CAO20.16.3(13.1)]*"
+        ]          
     , "What are the privileges and limitations of the single engine aeroplane class rating?" !-
-      DirectAnswer
-        "* pilot licence must demonstrate competency of part 61 MoS *[CASR61.400]*\n* flight review every 24 months, within 3 months of expiry, and valid to the end of that month *[CASR61.745]*"
+      DirectListAnswer
+        [
+          "pilot licence must demonstrate competency of part 61 MoS *[CASR61.400]*"
+        , "flight review every 24 months, within 3 months of expiry, and valid to the end of that month *[CASR61.745]*"
+        ]
     , "What requirement applies to take-off and landing distances?" !-
       DirectAnswer
-        "Add 15% to all take-off and landing distances for MTOW <= 2000kg *CAO20.7.4(6.1)*"
+        "Add 15% to all take-off and landing distances for MTOW <= 2000kg *[CAO20.7.4(6.1)]*"
     , "What documents must be carried?" !-
-      DirectAnswer
-        "Maintenance release, Pilot Operating Handbook, Aviation Medical Certificate"
+      DirectListAnswer
+        [
+          "Maintenance release"
+        , "Pilot Operating Handbook"
+        , "Aviation Medical Certificate"
+        , "Pilot Licence"
+        ]
     ]
 
 bobTaitChapterRevision ::
@@ -8938,7 +9013,7 @@ exams ::
   [Exam]
 exams =
   [
-    atcPreCiwrcuit
+    atcPreCircuit
   , atcPreArea
   , atcBAK
   , taitPreSolo
